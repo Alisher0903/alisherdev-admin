36a67c887a08a5bd62a6fa4d299bc6daed0eddc7
Alisher0903 <alishersodiqov09@gmail.com>
2025-07-03 16:18:01 +0500

    admin update

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const axiosBase = axios.create({
   baseURL: "https://alisherdev.up.railway.app",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
